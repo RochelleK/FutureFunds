38f21ac8ec8432f63bc8edbf6631ab8f8c4f0dca
Rochelle Katzman <zelrochelle@gmail.com>
2023-02-23 14:23:10 -0500

    routes work

---
+++ client/src/components/CaculatorContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const CaculatorContainer = () => {
+  return (
+    <div>
+      calc
+    </div>
+  );
+};
+
+export default CaculatorContainer;
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Home = () => {
+  // rsc is shortcut to create
+  return <div>Home</div>;
+};
+
+export default Home;
